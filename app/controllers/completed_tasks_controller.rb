d8e2a108fb68597a962d3619dbfbe288c9c74341
David Hale <dhale12@hotmail.co.uk>
2018-08-04 11:36:44 +0100

    Handle getting completed task by ID

--- app/controllers/completed_tasks_controller.rb
+++ app/controllers/completed_tasks_controller.rb
@@ -1,8 +1,19 @@
 class CompletedTasksController < ApplicationController
+  before_action :set_completed_task, only: [:show]
 
   def index
     @completed_tasks = CompletedTask.all
     json_response(@completed_tasks)
+  end
+
+  def show
+    json_response(@completed_task)
+  end
+
+  private
+
+  def set_completed_task
+    @completed_task = CompletedTask.find(params[:id])
   end
 
 end
--- spec/requests/completed_tasks_spec.rb
+++ spec/requests/completed_tasks_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'CompletedTasks API', type: :request do
   let!(:completed_tasks) { create_list(:completed_task, 10) }
   let(:completed_task_id) { completed_tasks.first.id }
 
-  describe 'GET /completed-tasks' do
+  describe 'GET /completed_tasks' do
     before { get '/completed_tasks' }
 
     it 'returns completed tasks' do
@@ -16,6 +16,35 @@ RSpec.describe 'CompletedTasks API', type: :request do
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
+
+  end
+
+  describe 'GET /completed_tasks/:id' do
+    before { get "/completed_tasks/#{completed_task_id}" }
+
+    context 'when the record exists' do
+      it 'returns the completed task' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(completed_task_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:completed_task_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find CompletedTask with 'id'=100/)
+      end
+    end
+
 
   end
 
