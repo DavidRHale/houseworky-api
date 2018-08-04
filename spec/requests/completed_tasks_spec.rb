049f7f93b0595acdf1e16ad413202312731e1b70
David Hale <dhale12@hotmail.co.uk>
2018-08-04 11:15:58 +0100

    Add CompletedTasks routes and alter test routes

--- spec/requests/completed_tasks_spec.rb
+++ spec/requests/completed_tasks_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'CompletedTasks API', type: :request do
   let(:completed_task_id) { completed_tasks.first.id }
 
   describe 'GET /completed-tasks' do
-    before { get '/completed-tasks' }
+    before { get '/completed_tasks' }
 
     it 'returns completed tasks' do
       expect(json).not_to be_empty
