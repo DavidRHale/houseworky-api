1c21a73c4bbc06367e8a14414d9fb101ca8d872c
David Hale <dhale12@hotmail.co.uk>
2018-08-05 05:59:21 +0100

    Add authentication utilities

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -9,7 +9,7 @@ class JsonWebToken
 
   def self.decode(token)
     body = JWT.decode(token, HMAC_SECRET)[0]
-    HasWithIndefferentAccess.new body
+    HashWithIndifferentAccess.new body
   rescue JWT::DecodeError => e
     raise ExceptionHandler::InvalidToken, e.message
   end
